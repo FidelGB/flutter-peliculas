e6143baa3c89eb3fe69e944ee9de44b600b0e148
Fidel GB <fidelgarcia.soft@gmail.com>
2021-05-08 13:13:08 -0500

    Se agrega instancia al widget de peliculas horizontal

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:peliculas/src/providers/pelicula_provider.dart';
 import 'package:peliculas/src/widgets/card_swipper_widget.dart';
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart'; 
 
 
 class HomePage extends StatelessWidget {
@@ -22,8 +23,10 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            _swiperTarjetas()
+            _swiperTarjetas(),
+            _footer(context)
           ],
         ),
       ),
@@ -43,6 +46,32 @@ class HomePage extends StatelessWidget {
             )
           );
       },
+    );
+  }
+
+  Widget _footer(BuildContext context){
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 20.0),
+            child: Text(
+              "Populares", 
+              style: Theme.of(context).textTheme.subtitle1),
+          ),
+          SizedBox(height: 5.0,),
+          FutureBuilder(
+            future: peliculasProvider.getPopulares(),
+            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+              return snapshot.hasData ?
+                MovieHorizontal(peliculas: snapshot.data) :
+                Center(child: CircularProgressIndicator());
+            },
+          ),
+        ]
+      ),
     );
   }
 }
