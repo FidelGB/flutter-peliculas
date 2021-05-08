5e5d25f8434d6f302062f8ca7bfc3452e21cfb20
Fidel GB <fidelgarcia.soft@gmail.com>
2021-05-08 00:18:16 -0500

    Agregado provider de peliculas

---
+++ lib/src/providers/pelicula_provider.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+
+class PeliculasProvider{
+  String _apikey = env["API_KEY"];
+  String _url = env["API_URL"];
+  String _languaje = env["LANGUAGE"];
+
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, "3/movie/now_playing", {
+      'api_key': _apikey,
+      'language': _languaje
+    });
+
+    final response = await http.get(url);
+    final decodedData = json.decode(response.body);
+
+    final peliculas = Peliculas.fromJsonList(decodedData["results"]);
+    
+    return peliculas.items;
+  }
+}
